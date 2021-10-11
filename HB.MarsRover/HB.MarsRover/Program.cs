de904433861c17eb3c56264b8a3b9eda4acd96ac
ysener <ycansener@gmail.com>
2021-10-11 13:17:23 +0300

    Main operations have been updated.

--- HB.MarsRover/HB.MarsRover/Program.cs
+++ HB.MarsRover/HB.MarsRover/Program.cs
@@ -21,15 +21,154 @@ namespace HB.MarsRover
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            int lineNumber = -1;
-            int robotCount = 0;
+            InputHelper inputHelper = new InputHelper();
+            IMobilityService mobilityService = new MobilityService();
+
+            char? selection = PrintInitializationOptionsAndGetUserInput();
+
+            if (selection != null && selection.Value == 'f')
+            {
+                InitializeFromFile(inputHelper, mobilityService);
+            }
+            else
+            {
+                InteractiveInitialization(inputHelper, mobilityService);
+            }
+
+            System.Console.ReadLine();
+        }
+
+        private static char? PrintInitializationOptionsAndGetUserInput()
+        {
+            Console.WriteLine($"Initialization Options:\nF: Initialize from input.txt file\nAny other input: Interactive Initialization\nPlease select: ");
+            string selection = Console.ReadLine();
+
+            return string.IsNullOrEmpty(selection) ? (char?)null : selection.ToLower().Trim()[0];
+        }
+
+        #region Interactive Initialization
+        private static void InteractiveInitialization(InputHelper inputHelper, IMobilityService mobilityService)
+        {
             IEnvironment environment = null;
             IRobot robot = null;
             IEnumerable<Enums.MovementCommand> commands = null;
+            int robotCount = 1;
 
-            InputHelper _inputHelper = new InputHelper();
-            IMobilityService _mobilityService = new MobilityService();
+            Console.WriteLine("INFO: Send 'X' to terminate the input sequence.");
 
+            while (true)
+            {
+                bool terminate;
+                if (environment == null)
+                {
+                    string environmentInfo = GetInfoFromUser("Environment Info (Ex. '5 5'): ", out terminate);
+
+                    if (terminate) break;
+
+                    try
+                    {
+                        environment = inputHelper.PopulateEnvironment(environmentInfo);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        environment = null;
+                    }
+
+                    if (environment == null)
+                    {
+                        System.Console.WriteLine(
+                            "Something wrong with the environment information. Please fill it again.");
+                    }
+                }
+                else if (robot == null)
+                {
+                    string robotInfo = GetInfoFromUser($"Robot {robotCount} Info (Ex. '1 2 N'): ", out terminate);
+
+                    if (terminate) break;
+
+                    try
+                    {
+                        robot = inputHelper.PopulateRobot(robotCount, robotInfo);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        robot = null;
+                    }
+
+                    if (robot == null)
+                    {
+                        System.Console.WriteLine(
+                            "Something wrong with the robot information. Please fill it again.");
+                    }
+                    else
+                    {
+                        robotCount++;
+                    }
+                }
+                else if (commands == null)
+                {
+                    string commandsInfo = GetInfoFromUser("Commands Info (Ex. 'LMLMLMLMM'): ", out terminate);
+
+                    if (terminate) break;
+
+                    try
+                    {
+                        commands = inputHelper.PopulateCommandList(commandsInfo);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        commands = null;
+                    }
+
+                    if (commands == null)
+                    {
+                        System.Console.WriteLine(
+                            "Something wrong with the commands information. Please fill it again.");
+                    }
+                }
+                else
+                {
+                    ExecuteCommands(commands, mobilityService, environment, robot);
+
+                    // Print latest location of the robot
+                    System.Console.WriteLine("Discovery has been completed.");
+                    Console.WriteLine($"{robot.PrintShortRobotInfo()}");
+                    commands = null;
+                    robot = null;
+                }
+            }
+
+            System.Console.WriteLine("Terminated by user intervention.");
+        }
+
+        private static string GetInfoFromUser(string message, out bool terminationRequested)
+        {
+            terminationRequested = false;
+            System.Console.WriteLine(message);
+            string input = Console.ReadLine();
+            input = input?.ToUpper();
+
+            if (input == "X")
+            {
+                terminationRequested = true;
+            }
+
+            return input;
+        }
+
+        #endregion
+
+        #region Initialize From File
+        private static void InitializeFromFile(InputHelper inputHelper, IMobilityService mobilityService)
+        {
+            IEnvironment environment = null;
+            IRobot robot = null;
+            IEnumerable<Enums.MovementCommand> commands = null;
+            int robotCount = 0;
+            int lineNumber = -1;
             string inputFileName = "input.txt";
             string filePath = Environment.CurrentDirectory + @"\" + inputFileName;
 
@@ -45,14 +184,14 @@ namespace HB.MarsRover
                 if (lineNumber == 0)
                 {
                     // No need to handle errors while populating the environment since if something is wrong with the environment, we should terminate the execution anyways.
-                    environment = _inputHelper.PopulateEnvironment(line);
+                    environment = inputHelper.PopulateEnvironment(line);
                 }
                 else
                 {
                     if (lineNumber % 2 == 1)
                     {
                         robotCount++;
-                        robot = HandleRobotLine(_inputHelper, line, _mobilityService, environment, robotCount);
+                        robot = HandleRobotLine(inputHelper, line, mobilityService, environment, robotCount);
                     }
                     else
                     {
@@ -67,7 +206,7 @@ namespace HB.MarsRover
                         // Green light to populate commands from the line
                         try
                         {
-                            commands = _inputHelper.PopulateCommandList(line);
+                            commands = inputHelper.PopulateCommandList(line);
                         }
                         catch (Exception e)
                         {
@@ -78,7 +217,7 @@ namespace HB.MarsRover
                             continue;
                         }
 
-                        ExecuteCommands(commands, _mobilityService, environment, robot);
+                        ExecuteCommands(commands, mobilityService, environment, robot);
 
                         // Print latest location of the robot
                         Console.WriteLine($"{robot.PrintShortRobotInfo()}");
@@ -86,11 +225,11 @@ namespace HB.MarsRover
                 }
 
             }
-
-            System.Console.WriteLine("Discovery has been completed.");
-            System.Console.ReadLine();
         }
 
+        #endregion
+
+        #region Common functions
         private static void ExecuteCommands(IEnumerable<Enums.MovementCommand> commands, IMobilityService _mobilityService, IEnvironment environment,
             IRobot robot)
         {
@@ -127,6 +266,7 @@ namespace HB.MarsRover
 
             return robot;
         }
+        #endregion
     }
 
 
